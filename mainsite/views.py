6caa32eb4ccc66a322148c026171f5498065aa8a
iiot-tbb <247687157@qq.com>
2019-11-09 17:25:50 +0800

    2019119django_study

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Post
 from datetime import datetime
@@ -14,3 +14,11 @@ def homepage(request):
     #return HttpResponse(post_lists)
     now =datetime.now()
     return render(request, 'index.html',locals())
+
+def showpost(request,slug):
+    try:
+        post =Post.objects.get(slug=slug)
+        if post != None:
+            return render(request, 'post.html',locals())
+    except:
+        return redirect('/')
